33649af626215f18f0fc3f4b40808cfc39c11ff0
sameer8saini <sameer8saini@gmail.com>
2022-07-11 17:34:45 +1200

    Add project files.

---
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+    }
+}
---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid BlogPostId { get; set; }
+    }
+}
